94a81f1c6dd340fec6b82991e868bb765f74f5ef
anca9150 <mpancaprasetya@gmail.com>
2026-03-18 00:03:04 +0700

    feat: tambah autentikasi dan pisah sidebar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,55 +1,47 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 
-<title>Login - POS</title>
+    <title>Login - POS</title>
 
-@vite(['resources/css/app.css','resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
 </head>
 
 <body class="auth-body">
-
-<div class="auth-container">
-
-<div class="auth-card">
-
-<h2>Login POS</h2>
-
-<form method="POST" action="/login">
-
-@csrf
-
-<div class="form-group">
-
-<label>Email</label>
-<input type="email" name="email" required>
-
-</div>
-
-<div class="form-group">
-
-<label>Password</label>
-<input type="password" name="password" required>
-
-</div>
-
-<button class="auth-btn">
-Login
-</button>
-
-<p class="auth-link">
-
-Belum punya akun?
-<a href="/register">Register</a>
-
-</p>
-
-</form>
-
-</div>
-
-</div>
-
+    <div class="auth-container">
+        <div class="auth-card">
+            <h2>Login POS</h2>
+            @if ($errors->any())
+                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded mb-4"">
+                    <ul class="list-disc pl-5">
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="form-group">
+                    <label>Email</label>
+                    <input type="email" name="email">
+                </div>
+                <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" name="password">
+                </div>
+                <button class="auth-btn">
+                    Login
+                </button>
+                <p class="auth-link">
+                    Belum punya akun?
+                    <a href="/register">Register</a>
+                </p>
+            </form>
+        </div>
+    </div>
 </body>
+
 </html>
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,86 +1,33 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 
-<title>POS Kasir</title>
+    <title>POS Kasir</title>
 
-@vite(['resources/css/app.css','resources/js/app.js'])
-
-<link rel="stylesheet"
-href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
 </head>
 
 <body>
 
-<div class="layout">
+    <div class="layout">
 
-<!-- SIDEBAR -->
+        <!-- SIDEBAR -->
 
-<div class="sidebar">
+        <x-sidebar></x-sidebar>
 
-<div class="logo">
-<i class="fa-solid fa-cash-register"></i>
-<span>POS</span>
-</div>
+        <!-- CONTENT -->
 
-<ul class="menu">
+        <div class="content">
 
-<li>
-<a href="/">
-<i class="fa-solid fa-chart-line"></i>
-<span>Dashboard</span>
-</a>
-</li>
+            @yield('content')
 
-<li>
-<a href="/transaksi">
-<i class="fa-solid fa-cart-shopping"></i>
-<span>Transaksi</span>
-</a>
-</li>
+        </div>
 
-<li>
-<a href="/stok">
-<i class="fa-solid fa-box"></i>
-<span>Stok Barang</span>
-</a>
-</li>
-
-<li>
-<a href="/laporan">
-<i class="fa-solid fa-file-lines"></i>
-<span>Laporan</span>
-</a>
-</li>
-
-<li class="logout">
-
-<form method="POST" action="{{ route('logout') }}">
-@csrf
-<button type="submit">
-
-<i class="fa-solid fa-right-from-bracket"></i>
-<span>Logout</span>
-
-</button>
-</form>
-
-</li>
-
-</ul>
-
-</div>
-
-<!-- CONTENT -->
-
-<div class="content">
-
-@yield('content')
-
-</div>
-
-</div>
+    </div>
 
 </body>
+
 </html>
