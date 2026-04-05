25028627c1ffd78698118e5376688e0969466b06
anca9150 <mpancaprasetya@gmail.com>
2026-04-05 23:04:34 +0700

    tambah role dan tenant_id di users

---
+++ database/migrations/2026_04_05_155714_add_role_and_tenant_to_users.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->enum('role', ['admin', 'tenant', 'kasir'])->default('kasir');
+            $table->foreignId('tenant_id')->nullable()->constrained()->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role');
+            $table->dropForeign(['tenant_id']);
+            $table->dropColumn('tenant_id');
+        });
+    }
+};
